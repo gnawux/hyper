0b893bd2dc8a76d2a93bef418e448a35e530e154
Wang Xu <gnawux@gmail.com>
2016-11-22 01:15:35 +0800

    [refactor] fix rename container
    Signed-off-by: Wang Xu <gnawux@gmail.com>

--- daemon/rename.go
+++ daemon/rename.go
@@ -7,10 +7,6 @@ import (
 )
 
 func (daemon *Daemon) ContainerRename(oldname, newname string) error {
-	if err := daemon.Daemon.ContainerRename(oldname, newname); err != nil {
-		return err
-	}
-
 	p, id, ok := daemon.PodList.GetByContainerIdOrName(oldname)
 	if !ok {
 		err := fmt.Errorf("caonnot find pod contains container %s to rename", oldname)
